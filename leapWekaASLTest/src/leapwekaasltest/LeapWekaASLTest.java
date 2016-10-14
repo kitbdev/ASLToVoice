7d07800dc5ede7c5207b248c0dc2e6bda0a5f70d
ian <ianb96@gmail.com>
2016-10-14 10:34:00 -0400

    listener ready

--- leapWekaASLTest/src/leapwekaasltest/LeapWekaASLTest.java
+++ leapWekaASLTest/src/leapwekaasltest/LeapWekaASLTest.java
@@ -11,7 +11,8 @@ public class LeapWekaASLTest {
         Controller controller = new Controller();
         
         controller.addListener(listener);
-//        controller.frame();
+        
+        //while(true){}
         
         System.out.println("Press Enter to quit...");
         try {
@@ -19,6 +20,7 @@ public class LeapWekaASLTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println("Quiting");
         controller.removeListener(listener);
     }
     
@@ -32,5 +34,10 @@ class SampleListener extends Listener {
 
     public void onFrame(Controller controller) {
         System.out.println("Frame available");
+        Frame frame = controller.frame();
+        System.out.println("Frame id: " + frame.id()
+                   + ", timestamp: " + frame.timestamp()
+                   + ", hands: " + frame.hands().count()
+                   + ", fingers: " + frame.fingers().count());
     }
 }
